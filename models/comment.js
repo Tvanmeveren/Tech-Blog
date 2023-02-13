01d8157c39ab75ed9147db35c4d8b7ac3da94b5f
Tvanmeveren <Tyvan07@yahoo.com>
2023-02-12 23:03:31 -0600

    Added code for seeds and public and reworked some code.

--- models/comment.js
+++ models/comment.js
@@ -1,46 +1,48 @@
-const {Model, DataTypes} = require('sequelize');
+
+const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 class Comment extends Model {}
 
 Comment.init(
-    {
+  {
     id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    comment_text: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-            len: [1]
-        }
+    content: {
+      type: DataTypes.STRING,
+    },
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
     },
     user_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        references: {
-            model: 'user',
-            key: 'id'
-        }
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
     },
     post_id: {
         type: DataTypes.INTEGER,
-        allowNull: false,
-        references: {
+        references:{
             model: 'post',
-            key: 'id'
+            key: 'id',
         }
-    } 
-    },
-    {
-        sequelize,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'comment'
     }
+  },
 
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment',
+  }
 );
 
 module.exports = Comment;
---
+++ public/login.js
@@ -0,0 +1,45 @@
+loginBtn.addEventListener("click", async () => {
+    event.preventDefault()
+  
+    let userLogin = document.getElementById("user-name-login").value.trim();
+    let passLogin = document.getElementById("password-login").value.trim();
+  
+    if (userLogin && passLogin) {
+        const response = await fetch('/api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({ 
+                name: userLogin, 
+                password: passLogin 
+            }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+        if (response.ok) {
+          console.log(response)
+            document.location.replace('/dashboard');
+        } else {
+            alert(response.statusText);
+        }
+    }
+  })
+  signupBtn.addEventListener("click", async () => {
+    event.preventDefault()
+    let userSignup = document.getElementById("user-name-signup").value.trim();
+    let passSignup = document.getElementById("password-signup").value.trim();    
+    
+    if (userSignup && passSignup) {
+        const response = await fetch('/api/users/', {
+            method: 'POST',
+            body: JSON.stringify({
+                name: userSignup,
+                password: passSignup
+            }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+  
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert(response.statusText);
+        }
+    }
+  })
